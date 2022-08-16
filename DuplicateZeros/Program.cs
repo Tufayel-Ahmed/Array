512999c0897e0f75ad124327d5a9c7404b0c4de0
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-16 21:57:25 +0600

    Duplicate Zeros Problem Commit

---
+++ DuplicateZeros/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DuplicateZeros
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] arr = new int[] { 1, 0, 5, 4, 0 };
+            solution.DuplicateZeros(arr);
+        }
+    }
+    public class Solution
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+            if (!arr.Contains(0) || arr.Length < 2)
+            {
+                return;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    int j;
+                    for (j = arr.Length - 1; j > i + 1; j--)
+                    {
+                        arr[j] = arr[j - 1];
+                    }
+                    arr[j] = 0;
+                    i++;
+                }
+            }
+            Console.WriteLine(String.Join(",", arr));
+        }
+    }
+}
