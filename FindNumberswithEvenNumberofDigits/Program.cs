31ccb7defda3d7016a653e26adac796b364dd153
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-16 17:57:02 +0600

    Find Numbers with Even Number of Digits Problem Commit

---
+++ FindNumberswithEvenNumberofDigits/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FindNumberswithEvenNumberofDigits
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Solution solution = new Solution();
+            int[] array = new int[] { 16, 556, 65646, 645564, 3256464 };
+            int result = solution.FindNumbers(array);
+            Console.WriteLine(result);
+        }
+    }
+    public class Solution
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int even = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = 0;
+                do
+                {
+                    count++;
+                    nums[i] = nums[i] / 10;
+                } while (nums[i] != 0);
+                if (count % 2 == 0)
+                {
+                    even++;
+                }
+            }
+            return even;
+        }
+    }
+}
