4d5c5bc09d628d893c58bd9b9b1cb2d7c0e439a9
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-16 18:16:28 +0600

    Squares of a Sorted Array Problem Commit

---
+++ SquaresofaSorted Array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SquaresofaSortedArray
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums = new int[] { -2, 1, 2, -5, 10 };
+            int[] result = solution.SortedSquares(nums);
+            
+            Console.WriteLine(String.Join(",", result));
+        }
+    }
+    public class Solution
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] * nums[i];
+            }
+            Array.Sort(nums);
+            return nums;
+        }
+    }
+}
