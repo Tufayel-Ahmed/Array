2421dd5c3dcfe1910279b53fec3f650fe74285d0
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-16 17:39:53 +0600

    Max Consecutive Ones Problem Commit

---
+++ MaxConsecutiveOnes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MaxConsecutiveOnes
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Solution solution = new Solution();
+            int[] array = new int[] {1, 0, 1, 1, 0 };
+
+            int result = solution.FindMaxConsecutiveOnes(array);
+            Console.WriteLine(result);
+        }
+    }
+    public class Solution
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int count = 0;
+            int max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    count++;
+                    max = Math.Max(max, count);
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            return max;
+        }
+    }
+}
