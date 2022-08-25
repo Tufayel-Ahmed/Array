b3c8f8a38cbd146f2ddb047c3ae66459c6199a2e
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-25 20:03:40 +0600

    Sort Array By Parity Problem Commit

---
+++ Move Zeroes/Move Zeroes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MoveZeres
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int[] nums = new int[] { 1, 5, 0, 8, 0, 0, 0, 7, 2};
+
+            Solution solution = new Solution();
+            solution.MoveZeroes(nums);
+        }
+    }
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int k = 0, temp;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    temp = nums[k];
+                    nums[k] = nums[i];
+                    nums[i] = temp;
+                    k++;
+                }
+            }
+            Console.WriteLine(String.Join(", ", nums));
+        }
+    }
+}
---
+++ SortArrayByParity/SortArrayByParity/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SortArrayByParity
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12};
+
+            Solution solution = new Solution();
+            int[] result = solution.SortArrayByParity(nums);
+            Console.WriteLine(String.Join(", ", result));
+        }
+    }
+
+    public class Solution
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            int k = 0, temp = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    (nums[k], nums[i]) = (nums[i], nums[k]);
+                    k++;
+                }
+            }
+            return nums;
+        }
+    }
+}
