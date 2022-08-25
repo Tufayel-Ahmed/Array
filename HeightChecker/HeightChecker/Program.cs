6cc667f2d6e8dc9c46483411bfe0d3ee414ac7c1
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-25 21:41:18 +0600

    Height Checker Problem Commit

---
+++ HeightChecker/HeightChecker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HeightChecker
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int[] heights = new int[] { 4, 2, 4, 3, 7, 1, 2, 9, 10};
+
+            Solution solution = new Solution();
+            int result = solution.HeightChecker(heights);
+            Console.WriteLine(result);
+
+        }
+    }
+
+    public class Solution
+    {
+        public int HeightChecker(int[] heights)
+        {
+            int count = 0;
+            int[] expected = new int[heights.Length];
+            Array.Copy(heights, expected, heights.Length);
+            Array.Sort(expected);
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if (expected[i] != heights[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
