bab5b69aa6917d5515fb4d2877445370096c3483
Tufayel Ahmed <tufayelahmed.edu@gmail.com>
2022-08-26 08:05:11 +0600

    Last Commit

--- ThirdMaximumNumber/ThirdMaximumNumber/Program.cs
+++ ThirdMaximumNumber/ThirdMaximumNumber/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ThirdMaximumNumber
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            Array.Sort(nums);
+            Console.WriteLine(String.Join(",", nums));
+        }
+    }
+}
